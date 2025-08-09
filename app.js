6936265af68cfd4ac2a414ec83fb90ff95bbf68f
Jhony Wilder Huaricallo Mamani <hmjhonywilder987@gmail.com>
2025-08-08 23:48:45 -0500

    funcion para agregar al amigo secreto

--- app.js
+++ app.js
@@ -1,2 +1,22 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
-let amigos = []
+// gracias a [] se guardaran los nombres]
+let amigos = [];
+
+function agregarAmigo(){
+    let input = document.getElementById("amigo");
+    let nombre = input.value.trim();
+}
+if (nombre==="") {
+    alert("ingrese el nombre.");
+    return;
+}
+
+amigos.push(nombre)
+console.log("lista de amigos", amigos);
+
+input.value = "";
+mostrarLista()
+
+function mostrarLista(){
+    let lista = document.getElementById("listaAmigo");
+    
+}
