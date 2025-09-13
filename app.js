f5401695e773f5188d3e38b4eb99ebe5be0430e9
Jhony Wilder Huaricallo Mamani <hmjhonywilder987@gmail.com>
2025-09-13 15:10:58 -0500

    cambios 2 e imagen y texto

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
 let amigos = []
+
+function agregarAmigo() {
+    let input = document.getElementById ("amigo")
+    let nombre = input.value.trim();
+
+
+if (nombre === "") {
+    alert ("¿y el nombre del sobreviviente?");
+    return
+}
+
+amigos.push(nombre)
+
+input.value = ""
+}
